05b4844c5128faa4563d84f8ce61ec5134f76ab2
Vinícius Moreira Centorbi <73324369+viniCentorbi@users.noreply.github.com>
2021-12-20 19:44:59 -0300

    Update Questao_05.py

--- Questões/Questao_05.py
+++ Questões/Questao_05.py
@@ -1,21 +1,26 @@
-len_string = len
-
 def reverse_string(string):
 
 	letras = []
 
+	#Percorre a string e adiciona cada caracterer na lista
 	for char in string:
 
 		letras.append(char)
 
+	#Recebe o tamanho da lista
 	num_letras = len(letras)
 
 	palavra = ''
+	indice = num_letras - 1
 
+	#Percorre a lista de caracteres
 	for i in range(num_letras):
 		
-		palavra = palavra + letras[num_letras-1]
-		num_letras-=1
+		#Concatena os caracteres da lista do último elemento para o primeiro
+		palavra = palavra + letras[indice]
+
+		#Decrementa o valor do índice para acessar a lista de trás para frente
+		indice-=1
 
 	print(palavra)
 
