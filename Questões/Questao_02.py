f176b2320621eb581b342b5e6749f4691975f088
Vinícius Moreira Centorbi <73324369+viniCentorbi@users.noreply.github.com>
2021-12-20 19:44:22 -0300

    Update Questao_02.py

--- Questões/Questao_02.py
+++ Questões/Questao_02.py
@@ -2,19 +2,25 @@ def fibonacci(numero):
 	penultimo = 0
 	ultimo = 1
 
+	#pertence recebe falso para no caso de um número negativo ser inserido
 	pertence = False
 
+	#Fará a sequência de Fibonacci até o número inserido
 	while penultimo <= numero:
 
+		#Verifica se o número pertence a sequência
 		if numero == penultimo or numero == ultimo:
-			pertence = True
-			break
+			pertence = True			
 		else:
 			pertence = False
 
+		#O próximo número da sequência vai receber a soma do último e penúltimo
 		proximo = penultimo + ultimo
 
+		#O último passa a ser o penúltimo
 		penultimo = ultimo
+
+		#E o próximo passar a ser o último
 		ultimo = proximo
 
 	return pertence
